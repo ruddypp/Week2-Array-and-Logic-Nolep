92e4cf7efea011ef014968a577ba07ee61626dae
Rudy Paningal <paningalrudy@gmail.com>
2025-01-23 08:11:33 +0700

    add add part 3

--- corat.js
+++ corat.js
@@ -1,2 +1,16 @@
-let array = [1,2,3,4,5];
-console.log(array);
+
+for(let i = 0 ; i < 7; i++){
+    let star = '';
+    for(let j = 0; j < i; j++){
+    star += '*';
+    }
+    console.log(star);
+}
+
+for( let k = 7; k >= 1; k--){
+    let star2= '';
+    for(let l = 0; l < k; l++){
+        star2 += '*';
+    }
+    console.log(star2);
+}
